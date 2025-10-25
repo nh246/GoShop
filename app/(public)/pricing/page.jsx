36a74ead64f99caa6a17cc591e78e49c0b62786b
nh246 <nazmul210130@diit.edu.bd>
2025-10-25 15:23:07 +0600

    testing

--- app/(public)/pricing/page.jsx
+++ app/(public)/pricing/page.jsx
@@ -3,7 +3,7 @@ export default function PricingPage() {
     return (
         <div className='mx-auto max-w-[700px] my-28'>
             {/* Pricing Table */}
-
+          <h1>Hood</h1>
         </div>
     )
 }
