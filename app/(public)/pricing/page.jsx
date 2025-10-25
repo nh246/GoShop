5c3b3c17180a61dc016475e6abdf8d5551e8ea94
nh246 <nazmul210130@diit.edu.bd>
2025-10-25 15:23:55 +0600

    git connection working

--- app/(public)/pricing/page.jsx
+++ app/(public)/pricing/page.jsx
@@ -3,7 +3,6 @@ export default function PricingPage() {
     return (
         <div className='mx-auto max-w-[700px] my-28'>
             {/* Pricing Table */}
-          <h1>Hood</h1>
         </div>
     )
 }
